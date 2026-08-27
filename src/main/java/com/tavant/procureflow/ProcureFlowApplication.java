3e87c792461a1eb6de2729f29079afede47a49cf
gautamtavant <gautam.raj@tavant.com>
2026-08-27 11:59:53 +0530

    first commit

---
+++ src/main/java/com/tavant/procureflow/ProcureFlowApplication.java
@@ -0,0 +1,13 @@
+package com.tavant.procureflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcureFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcureFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tavant/procureflow/ProcureFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tavant.procureflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcureFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
